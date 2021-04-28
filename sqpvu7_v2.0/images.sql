3a4ab724a24c776a270d7d8975f5420b8547374a
istvanblzs <72940501+istvanblzs@users.noreply.github.com>
2021-04-28 12:07:31 +0200

    Add files via upload

---
+++ sqpvu7_v2.0/images.sql
@@ -0,0 +1,9 @@
+USE `felhasznalok`;
+
+CREATE TABLE images (
+id int(11) AUTO_INCREMENT,
+    file_name varchar(255),
+    uploaded_on datetime,
+    PRIMARY KEY (id)
+)
+
---
+++ sqpvu7_v2.0/templates/pages/nyitvatartas.tpl.php
@@ -0,0 +1,36 @@
+<div style="background-image: url(/sqpvu7/images/dogo2.jpg);background-repeat: repeat;background-position: center; width:1800px;color:white;">
+<div data-mesh-id="comp-jwzz57gp3inlineContent-gridContainer" data-testid="mesh-container-content"><div id="comp-jwzz57gp4" class="_1Z_nJ" data-testid="richTextElement"><p class="font_0" style="text-align:center;line-height:1.25em;font-size:22px"><span class="color_11">Bármikor szívesen látjuk Önt és Kedvencét!</span></p></div><div id="comp-jwzz57gq" class="_1Z_nJ" data-testid="richTextElement"><h2 class="font_6" style="line-height: 1.146em; text-align: center; font-size: 50px;"><span style="font-size:50px;"><span class="color_11">NYITVATARTÁSI IDŐ</span></span></h2></div><div id="comp-jwzz57gq1" class="_1Z_nJ" data-testid="richTextElement"><p class="font_7" style="font-size:28px; line-height:1.25em; text-align:center"><span style="font-size:28px"><span class="color_11"><span style="font-weight:bold">&nbsp; &nbsp; &nbsp;</span><span style="font-weight:bold">Járóbeteg ellátás azaz<span style="text-decoration:underline"> <span style="text-shadow:rgba(255, 255, 255, 0.6) 1px 1px 1px, rgba(0, 0, 0, 0.6) -1px -1px 1px">időpontfoglalás nem szükséges:</span></span></span></span></span></p>
+
+<p class="font_7" style="font-size:16px; line-height:1.25em; text-align:center"><span style="font-size:16px"><span class="color_11"><span style="font-weight:bold"><span style="text-shadow:rgba(255, 255, 255, 0.6) 1px 1px 1px, rgba(0, 0, 0, 0.6) -1px -1px 1px">(Sürgősségi/életet veszélyeztető esetek soron kívüli ellátása)</span></span></span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px">&nbsp;</p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="font-weight:bold"><span class="color_11"><span style="text-shadow:rgba(0, 0, 0, 0.4) 0px 4px 5px"><span class="wixGuard">​</span></span></span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="font-weight:bold"><span class="color_11">Hétfő-Péntek</span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="font-weight:bold"><span class="color_11">15:00-20:00-ig &nbsp; &nbsp;</span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="font-weight:bold"><span class="color_11">Szombat</span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="font-weight:bold"><span class="color_11">9:00-12:00-ig</span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="font-weight:bold"><span class="wixGuard">​</span></span></p>
+
+<p class="font_7" style="font-size:28px; line-height:1.25em; text-align:center"><span style="font-size:28px"><span style="font-weight:bold"><span class="color_11"><span style="text-shadow:rgba(255, 255, 255, 0.6) 1px 1px 1px, rgba(0, 0, 0, 0.6) -1px -1px 1px"><span style="text-decoration:underline">Időpontegyeztetés alapján</span></span> mely telefonon lehetséges:</span></span></span></p>
+
+<p class="font_7" style="font-size:28px; line-height:1.25em; text-align:center"><span style="font-size:16px"><span class="color_11"><span style="font-weight:bold"><span style="text-shadow:rgba(255, 255, 255, 0.6) 1px 1px 1px, rgba(0, 0, 0, 0.6) -1px -1px 1px">(Sürgősségi/életet veszélyeztető esetek soron kívüli ellátása)</span></span></span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="font-weight:bold"><span class="wixGuard">​</span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="text-shadow:rgba(0, 0, 0, 0.4) 0px 4px 5px"><span style="font-weight:bold"><span class="color_11">Hétfő: 15:00-20:00</span></span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="text-shadow:rgba(0, 0, 0, 0.4) 0px 4px 5px"><span style="font-weight:bold"><span class="color_11">Kedd: 16:00-20:00</span></span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span style="text-shadow:rgba(0, 0, 0, 0.4) 0px 4px 5px"><span style="font-weight:bold"><span class="color_11">Szerda: 9:00-18:00</span></span></span></p>
+
+<p class="font_7" style="line-height:1.25em;font-size:20px"><span style="text-shadow:rgba(0, 0, 0, 0.4) 0px 4px 5px"><span style="font-weight:bold"><span class="color_11">&nbsp; &nbsp;</span></span></span></p>
+
+<p class="font_7" style="line-height:1.25em;text-align:center;font-size:20px"><span class="color_11"><span style="font-weight:bold">​</span></span></p>
+<br><br><br><br><br>
+</div>
